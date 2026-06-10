c99981d23b530057904c8fcc789cc5af8c306e6b
b22036tdchanmacgroup-source <b22036.tdc.hanmacgroup@gmail.com>
2026-06-11 02:40:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HM_MIS/',  // ← 이 줄 추가 (저장소명이 HM_MIS)
+  base: '/HM_MISystem/',  // ← 이 줄 추가 (저장소명이 HM_MIS)
   plugins: [react()],
   server: {
     port: 5173,
